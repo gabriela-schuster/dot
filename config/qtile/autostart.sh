9f89fa6723bd2656981cb0514b01ba0433b1ce8e
indigo31 <71771647+indigo31@users.noreply.github.com>
2021-02-20 14:45:09 -0300

    Update autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-feh --bg-fill /home/indigo/Pictures/sunset.jpg &
-compton -b &
+feh --bg-fill /home/indigo/Pictures/mon.png &
+picom -b
+setxkbmap -model abnt2 -layout br &
+%(xrandr)s &
+xrandr --output HDMI1 --mode 2560x1080_60.00
