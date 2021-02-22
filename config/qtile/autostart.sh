962cb2e427364e9ab2324862bcc878be210b6e12
indigo31 <71771647+indigo31@users.noreply.github.com>
2021-02-22 13:41:37 -0300

    Update autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -2,7 +2,8 @@
 feh --bg-fill /home/indigo/Pictures/mon.png &
 picom -b
 setxkbmap -model abnt2 -layout br &
-%(xrandr)s &
+xrandr --newmode "2560X1080_60.00" 230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
+xransr --addmode HDMI1 2560x1080_60.00
 xrandr --output HDMI1 --mode 2560x1080_60.00
 # monitor setting cmd sequence:
 # cvt 2560 1080 ## resolution we want
