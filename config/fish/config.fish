45e86bf585b2e8e29644d2e73af3e71277103846
Gabriela S. Tomazzi <gabtomazzi@gmail.com>
2021-06-25 14:35:28 -0300

    Delete config/fish directory

--- config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-set fish_color_normal '#cccccc'
-set fish_color_autosuggestion '#7f7f84'
-set fish_color_command '#86baba'
-set fish_color_error '#8d8277'
-set fish_color_param '#7f7f84'
-
-starship init fish | source
