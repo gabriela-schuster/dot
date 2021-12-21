1d03932b3190aaba77c9828cff0940efe5ed10ce
Gabriela S. Tomazzi <gabtomazzi@gmail.com>
2021-12-21 09:47:08 -0300

    Delete config/qtile directory

--- config/qtile/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-nitrogen --restore
-#feh --bg-fill /home/indigo/Pictures/astronaut.jpg &
-picom -b
-setxkbmap -model abnt2 -layout br
-xrandr --newmode "2560x1080_60.00"  230.00  2560 2720 2992 3424  1080 1083 1093 1120 -hsync +vsync
-xrandr --addmode HDMI1 2560x1080_60.00
-xrandr --output HDMI1 --mode 2560x1080_60.00
